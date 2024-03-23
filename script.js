d7c126dacf9bc6f9d4a962492d5f7c699bba239e
Imrah Iqbal <devroomdev@gmail.com>
2024-03-24 01:13:34 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let string="";
+let button=document.querySelectorAll('.btn');
+Array.from(button).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+      if (e.target.innerHTML=='=') {
+          string=eval(string);
+          document.querySelector('input').value=string;
+      }
+      else if (e.target.innerHTML=='AC') {
+        string="";
+        document.querySelector('input').value=string;
+      }
+      else if (e.target.innerHTML=='Del') {
+        string= string.slice(0, -1);
+        document.querySelector('input').value=string;
+      }
+      else{
+          console.log(e.target);
+          string=string+e.target.innerHTML;
+          document.querySelector('input').value=string;
+        }
+        })
+})
